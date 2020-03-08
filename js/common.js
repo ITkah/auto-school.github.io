fa58d2a9cb4ea9b32d67fb40a7a73a0d112023ba
ITkah <ilatkacenko132@gmail.com>
2020-03-08 22:44:51 +0200

    add 4 section

--- js/common.js
+++ js/common.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+
+    $('div.tabs__caption').on('click', 'div:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+
     var header = $("header");
 
     $(window).scroll(function() {
@@ -17,6 +24,111 @@ $(document).ready(function() {
 
     $(".btn-call-menu").on("click", function() {
         $("nav").slideToggle(100);
+    });
+
+    $('.single-item').slick({
+        infinite: true,
+        nav: false,
+        dots: true,
+        margin: 10
+    });
+
+    $('.teacher-item').slick({
+      infinite: true,
+      nav: true,
+      dots: false,
+      margin: 20,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      prevArrow: "<img src='img/icon/left.png' class='prev' alt='1'>",
+      nextArrow: "<img src='img/icon/right.png' class='next' alt='2'>",
+      responsive: [
+        {
+          breakpoint: 990,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            nav: false,
+            margin: 10,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            nav: false,
+            margin: 10,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            nav: false,
+            margin: 10,
+            dots: true
+          }
+        }
+      ]
+  });
+
+    $('.review-item').slick({
+        infinite: true,
+        nav: true,
+        dots: false,
+        margin: 10,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: "<img src='img/icon/left.png' class='prev' alt='1'>",
+        nextArrow: "<img src='img/icon/right.png' class='next' alt='2'>",
+        responsive: [
+          {
+            breakpoint: 990,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              nav: false,
+              margin: 10,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              nav: false,
+              margin: 10,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              nav: false,
+              margin: 10,
+              dots: true
+            }
+          }
+        ]
+    });
+
+    $('.image-popup-vertical-fit').magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+      mainClass: 'mfp-img-mobile',
+      image: {
+        verticalFit: true
+      }
+      
     });
 
 });
